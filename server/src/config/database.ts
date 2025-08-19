92a7daac1f09bb194b1a0914ad8a28a328988c06
Akinkunmi Akinbo <akinkunmi1.akinbo@gmail.com>
2025-08-19 01:19:25 -0400

    initial database config

---
+++ server/src/config/database.ts
@@ -0,0 +1,8 @@
+import knex from 'knex';
+
+const config = require('../../knexfile');
+
+const environment = process.env.NODE_ENV || 'development';
+const db = knex(config[environment]);
+
+export default db;
